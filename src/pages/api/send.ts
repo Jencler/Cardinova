bb49268f1dc3b43e7b99d4e3ed14cba63b5ccdc7
jencler huaches <jhuachesvasquez44@gmail.com>
2025-02-10 13:00:58 -0500

    video

--- src/pages/api/send.ts
+++ src/pages/api/send.ts
@@ -1,9 +1,21 @@
 import type { APIRoute } from "astro";
+import { Resend } from "resend";
+
+const resend = new Resend(import.meta.env.RESEND_API_KEY);
 
 export const GET: APIRoute = async () => {
-  return new Response(
-    JSON.stringify({
-      success: true,
-    })
-  );
+  //send an email
+
+  const { data, error } = await resend.emails.send({
+    from: 'Acme <onboarding@resend.dev>',
+    to: ['delivered@resend.dev'],
+    subject: "It Works!",
+    html: `<p>Hello cardinova</p>`,
+  });
+
+  if (error) {
+    return new Response(JSON.stringify(error));
+  }
+
+  return new Response(JSON.stringify(data));
 };
